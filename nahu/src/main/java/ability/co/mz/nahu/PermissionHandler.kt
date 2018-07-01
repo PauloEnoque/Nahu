8157b678bb708974ed17cef2b97d5881b74bb959
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-07-01 23:19:48 +0200

    fix: Request Multiple Permission & typo on property name - fix PauloEnoque/Nahu#5 - Also corrected the typo on the permissionExplanation property

--- nahu/src/main/java/ability/co/mz/nahu/PermissionHandler.kt
+++ nahu/src/main/java/ability/co/mz/nahu/PermissionHandler.kt
@@ -12,11 +12,11 @@ import android.widget.Toast
  * Created by GHOST on 3/3/2018.
  */
 class PermissionHandler {
-    var permission: String? = null
+    var permissions: Array<String>? = null
     var activity: Activity? = null
     private var context: Context? = null
     var requestCode: Int? = null
-    var permissionExplaination: String? = null
+    var permissionExplanation: String? = null
 
     fun then(block: () -> Unit) {
 
@@ -24,31 +24,28 @@ class PermissionHandler {
             context = activity?.baseContext
 
             // Here, thisActivity is the current activity
-            if (ContextCompat.checkSelfPermission(context!!,
-                    permission ?: "")
-                    != PackageManager.PERMISSION_GRANTED) {
+            if (!hasPermissions()) {
 
                 // Should we show an explanation?
-                if (ActivityCompat.shouldShowRequestPermissionRationale(activity!!,
-                        permission ?: "")) {
+                if (shouldShowExplanation()) {
 
                     // Show an explanation to the user *asynchronously* -- don't block
                     // this thread waiting for the user's response! After the user
                     // sees the explanation, try again to request the permission.
-                    permissionExplaination?.let {
-                        Toast.makeText(context, permissionExplaination, Toast.LENGTH_LONG).show()
-
+                    permissionExplanation?.let {
+                        Toast.makeText(context, permissionExplanation, Toast.LENGTH_LONG).show()
                     }
+
 //                  Request the permission again
                     ActivityCompat.requestPermissions(activity!!,
-                            arrayOf(permission),
+                            permissions!!,
                             requestCode!!)
 
                 } else {
 
                     // No explanation needed, we can request the permission.
                     ActivityCompat.requestPermissions(activity!!,
-                            arrayOf(permission),
+                            permissions!!,
                             requestCode!!)
 
 
@@ -65,10 +62,31 @@ class PermissionHandler {
 
     private fun noNullsFound(): Boolean {
         if (activity == null)    throw ComponentNotSetException(component = "Activity")
-        if (permission == null)  throw ComponentNotSetException(component = "Permission")
+        if (permissions == null || permissions?.size == 0) {
+            throw ComponentNotSetException(component = "Permissions")
+        }
         if (requestCode == null) throw ComponentNotSetException(component = "Request Code")
 
         return true
+    }
+
+    private fun hasPermissions(): Boolean {
+        for (permission in permissions!!) {
+            if (ContextCompat.checkSelfPermission(context!!, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun shouldShowExplanation(): Boolean {
+        for (permission in permissions!!) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity!!, permission)) {
+                return true
+            }
+        }
+        return false
     }
 
 }
