18bfe7a2445c329ef3a77277e465de638499db10
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-07-12 01:28:09 +0200

    feat: let Nahu handle PermissionResults - Create ResultHandler class with functions triggered with a list of granted/denied Permissions
    - Create DSL function to handle Permission Result
    
    - Update onRequestPermissionsResult on the sample app to use the new feature

--- app/src/main/java/ability/co/mz/nahuexample/MainActivity.kt
+++ app/src/main/java/ability/co/mz/nahuexample/MainActivity.kt
@@ -1,5 +1,6 @@
 package ability.co.mz.nahuexample
 
+import ability.co.mz.nahu.handlePermissionsResult
 import ability.co.mz.nahu.permissionManager
 import android.Manifest
 import android.content.Intent
@@ -7,6 +8,8 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -30,9 +33,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when(grantResults[0]){
-            PackageManager.PERMISSION_GRANTED -> openDialerApp()
+
+        handlePermissionsResult {
+            this.requestCode = requestCode
+            this.permissions = permissions
+            this.grantResults = grantResults
+
+            onPermissionGranted { permission ->
+                when (permission) {
+                    Manifest.permission.CALL_PHONE -> openDialerApp()
+                }
+            }
+
+            onPermissionDenied { permission ->
+                when (permission) {
+                    Manifest.permission.CALL_PHONE -> {
+                        Toast.makeText(applicationContext, "Cant open dialler without permission",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
         }
+
     }
 
     private fun openDialerApp() {
